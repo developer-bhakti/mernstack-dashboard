e20073c69a8cd8c74099f1851f4c46e39736cefb
purnale <bhaktipurnale89@gmail.com>
2025-07-02 22:22:15 +0530

    login

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Componets/Auth/Login'
+import EmployeeDashboard from './Componets/Dashboard/EmployeeDashboard'
+
+const App = () => {
+  return (
+    <>
+      {/* <Login /> */}
+      <EmployeeDashboard />
+    </>
+  )
+}
+
+export default App
