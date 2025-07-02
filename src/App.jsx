cdd7dacc6e27ec7cfaf47b793e29d04ec889a883
purnale <bhaktipurnale89@gmail.com>
2025-07-02 22:57:18 +0530

    admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Login from './Componets/Auth/Login'
 import EmployeeDashboard from './Componets/Dashboard/EmployeeDashboard'
+import AdminDashboard from './Componets/Dashboard/AdminDashboard'
 
 const App = () => {
   return (
     <>
       {/* <Login /> */}
-      <EmployeeDashboard />
+      {/* <EmployeeDashboard /> */}
+      {/* <AdminDashboard /> */}
     </>
   )
 }
