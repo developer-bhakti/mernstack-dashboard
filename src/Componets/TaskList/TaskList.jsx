b7f3201aa9124068513d50448ced5ee81f294fd0
purnale <bhaktipurnale89@gmail.com>
2025-07-08 15:01:43 +0530

    add

--- src/Componets/TaskList/TaskList.jsx
+++ src/Componets/TaskList/TaskList.jsx
@@ -1,54 +1,34 @@
-import React from 'react'
+import React from "react";
+import AcceptTask from "./AcceptTask";
+import NewTask from "./NewTask";
+import CompleteTask from "./CompleteTask";
+import FailledTask from "./FailledTask";
 
-const TaskList = () => {
+const TaskList = ({ data }) => {
   return (
-    <div id='tasklist' className='h[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full mt-10 py-5'>
-      <div className='flex-shrink-0 h-full w-[300px] bg-red-400 rounded-xl'>
-         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-            <h4 className='text-sm'>20 feb 2024</h4>
-         </div>
-         <h2 className='mt-5 text-2xl font-semibold'>Make a Video</h2>
-         <p className='text-sm mt-2'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         </p>
+    <div
+      id="tasklist"
+      className="h[50%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full mt-10 py-5"
+    >
+      {data.tasks.map((elem,idx) => {
+        if (elem.active) {
+          return <AcceptTask key={idx} data={elem} />;
+        }
 
-      </div>
+        if (elem.newTask) {
+          return <NewTask key={idx} data={elem} />;
+        }
 
-      <div className='flex-shrink-0 h-full w-[300px] bg-yellow-400 rounded-xl'>
-         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-            <h4 className='text-sm'>20 feb 2024</h4>
-         </div>
-         <h2 className='mt-5 text-2xl font-semibold'>Make a Video</h2>
-         <p className='text-sm mt-2'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         </p>
-      </div>
+        if (elem.completeTask) {
+          return <CompleteTask key={idx} data={elem} />;
+        }
 
-      <div className='flex-shrink-0 h-full w-[300px] bg-green-400 rounded-xl'>
-         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-            <h4 className='text-sm'>20 feb 2024</h4>
-         </div>
-         <h2 className='mt-5 text-2xl font-semibold'>Make a Video</h2>
-         <p className='text-sm mt-2'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         </p>
-      </div>
-
-      <div className='flex-shrink-0 h-full w-[300px] bg-blue-400 rounded-xl'>
-         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-            <h4 className='text-sm'>20 feb 2024</h4>
-         </div>
-         <h2 className='mt-5 text-2xl font-semibold'>Make a Video</h2>
-         <p className='text-sm mt-2'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-         </p>
-      </div>
+        if (elem.failledTask) {
+          return <FailledTask key={idx} data={elem} />;
+        }
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
--- src/Componets/other/Header.jsx
+++ src/Componets/other/Header.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+// import React, { useState } from "react";
+import { setLocalStorage } from "../../Utils/LocalStorage";
 
-const Header = () => {
+const Header = (props) => {
+ 
+  // const [username, setUserName] = useState('');
+
+  // if(!data){
+  //   setUserName('Admin')
+  // }else{
+  //   setUserName(data.firstname)
+  // }
+  const logOutUser = ()=>{
+     localStorage.setItem('loggedInUser','')
+     props.changeUser('')
+    //  window.location.reload()
+  }
+
   return (
-    <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello  <br /> <span className='text-3xl font-semibold'>Bhaktiiii👋</span></h1>
-        <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+    <div className="flex items-end justify-between">
+      <h1 className="text-2xl font-medium">
+        Hello <br />{" "}
+        <span className="text-3xl font-semibold">username👋</span>
+      </h1>
+      <button onClick={logOutUser} className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm">
+        Log Out
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
